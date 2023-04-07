98f00213a4e3300b0f167538e9c1e71e93fae98f
Episqol <khomchykdmitry@gmail.com>
2023-04-07 15:05:42 +0300

    CHR-6 create presale contract wrapper for testing purposes

---
+++ contracts/test/CHRPresale.testnet.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "../CHRPresale.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract CHRPresaleTestnet is CHRPresale {
+    using SafeERC20 for IERC20;
+
+    constructor(
+        address _saleToken,
+        address _oracle,
+        address _usdt,
+        uint256 _saleStartTime,
+        uint256 _saleEndTime,
+        uint256[4] memory _limitPerStage,
+        uint256[4] memory _pricePerStage
+    ) CHRPresale(_saleToken, _oracle, _usdt, _saleStartTime, _saleEndTime, _limitPerStage, _pricePerStage){}
+
+    function t_resetUser(address _user) public {
+        hasClaimed[_user] = false;
+        purchasedTokens[_user] = 0;
+    }
+
+    function t_claimAndReset() external whenNotPaused {
+        if(block.timestamp < claimStartTime || claimStartTime == 0) revert InvalidTimeframe();
+        if(hasClaimed[_msgSender()]) revert AlreadyClaimed();
+        uint256 amount = purchasedTokens[_msgSender()];
+        if(amount == 0) revert NothingToClaim();
+        hasClaimed[_msgSender()] = true;
+        IERC20(saleToken).safeTransfer(_msgSender(), amount);
+        emit TokensClaimed(_msgSender(), amount, block.timestamp);
+        t_resetUser(_msgSender());
+    }
+}
