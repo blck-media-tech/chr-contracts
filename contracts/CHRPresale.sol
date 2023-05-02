383103ae7cbf3614eedbb0bbcb3f719cab60f738
Episqol <khomchykdmitry@gmail.com>
2023-05-02 19:34:22 +0300

    CHR-6 copied necessary openzeppelin contracts

--- contracts/CHRPresale.sol
+++ contracts/CHRPresale.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./openzeppelin/access/Ownable.sol";
+import "./openzeppelin/security/Pausable.sol";
+import "./openzeppelin/security/ReentrancyGuard.sol";
+import "./openzeppelin/token/ERC20/IERC20.sol";
+import "./openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
 import "./interfaces/IChainlinkPriceFeed.sol";
 import "./interfaces/IPresale.sol";
--- contracts/CHRToken.sol
+++ contracts/CHRToken.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./openzeppelin/token/ERC20/ERC20.sol";
+import "./openzeppelin/access/Ownable.sol";
 
 contract CHRToken is ERC20, Ownable {
     constructor(uint256 _initialSupply) ERC20("Chancer", "CHR") Ownable() {
